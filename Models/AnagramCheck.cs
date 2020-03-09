c782524179fbc0f310cc64ee2298a139db070e84
Jiwon <jiwon1.han@gmail.com>
2020-03-09 13:13:55 -0700

    partial anagram for consecutive chars

--- Models/AnagramCheck.cs
+++ Models/AnagramCheck.cs
@@ -13,7 +13,7 @@ namespace Anagram.Models
         public static List<string> ConvertedAnagrams = new List<string>() {};
         public static List<string> ConvertedAnagrams2 = new List<string>() {};
 
-        public static string trimedAnagram;
+        public static List<string> ConsecutiveAnagram = new List<string>() {};
         public AnagramCheck(string initialWord)
         {
           InitialWord = initialWord;
@@ -37,15 +37,25 @@ namespace Anagram.Models
         }
         public void AnagramChecker()
         {
+          Console.WriteLine(ConsecutiveAnagram[0]);
+          Console.WriteLine(ConsecutiveAnagram[1]);
+
           for (int i = 0; i < ConvertedAnagrams.Count; i++)
           {
             if (Result == ConvertedAnagrams[i])
             {
               Console.WriteLine($"'{OriginalAnagrams[i]}' is an Anagram for '{InitialWord}'");
             }
-            else if (Result == ConvertedAnagrams2[i]) {
+            else if (Result == ConvertedAnagrams2[i]) 
+            {
               Console.WriteLine($"'{OriginalAnagrams[i]} is a partially Anagram for '{InitialWord}'");
-            } else {
+            } 
+            else if (ConsecutiveAnagram[i] == "true")
+            {
+              Console.WriteLine($"'{OriginalAnagrams[i]} is a partially Anagram for '{InitialWord}'");
+            }
+            else 
+            {
               Console.WriteLine($"No match for '{OriginalAnagrams[i]}'");
             }
           }
@@ -53,31 +63,55 @@ namespace Anagram.Models
 
         public void userPartialAnagram(string userAnagram)
         {   
-            string converted = "";
+            
+            List<int> userCharIndexNumbers = new List<int>(){};
+            List<int> anagramCharIndexNumbers = new List<int>(){};
+            int userCharIndex = 0;
+            int anagramCharIndex = 0;
+            int userAnagramLength = 0;
+
+            char[] arrayUserAnagram = userAnagram.ToCharArray();
+            Array.Sort(arrayUserAnagram);
+            string sortedUserAnagram = new string(arrayUserAnagram);
+              
+              for (int i = 1; i < Result.Length; i++)
+                 
+                { if(Result[i-1] == Result[i])
+                  userCharIndex++;
+                } userCharIndexNumbers.Add(userCharIndex);
+
+              for (int y = 1; y < sortedUserAnagram.Length; y++ )
+                   { if(sortedUserAnagram[y-1] == sortedUserAnagram[y])
+                     anagramCharIndex++;
+                   } anagramCharIndexNumbers.Add(anagramCharIndex);
+
+              for (int z = 0; z < userAnagram.Length; z++)
+                    {
+                      if(Result.Contains(userAnagram[z]))
+                      userAnagramLength++;
+                    }
+              string converted = "";
+
               for (int x = 0; x <userAnagram.Length; x++)
               {
-                if (!( Result.Contains(userAnagram[x])))
+                if (!(Result.Contains(userAnagram[x])))
                 {
                   userAnagram = userAnagram.Replace(userAnagram[x],' ').Trim();
                   char [] partialAnagramCheck = userAnagram.ToCharArray();
                   Array.Sort(partialAnagramCheck);
                   converted = new string(partialAnagramCheck);
                   converted = converted.ToLower();
-            
                 }
-              }
-              ConvertedAnagrams2.Add(converted);
-           }
+              }ConvertedAnagrams2.Add(converted);             
 
-        public void PartialAnagramChecker()
-        {
-          for (int i = 0; i <ConvertedAnagrams2.Count; i++)
-          {
-            if (Result == ConvertedAnagrams2[i])
-            {
-              Console.WriteLine($"'{OriginalAnagrams[i]} is a partially Anagram for '{InitialWord}'");
-            }
-          }
-        }
+              if (userAnagram.Length == userAnagram.Length && anagramCharIndex > userCharIndex)
+                {
+                  ConsecutiveAnagram.Add("true");
+                }          
+              else if (!(userAnagram.Length == userAnagram.Length && anagramCharIndex > userCharIndex))
+                {
+                  ConsecutiveAnagram.Add("false");
+                }
+           }
   }
 }
